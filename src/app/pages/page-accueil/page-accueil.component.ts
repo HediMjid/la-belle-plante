d23ef1d3ff6a7ca09c26aa818b12d057bcb2c44a
Hedi MJID <hedi.mjid@gmail.com>
2021-12-15 17:30:17 +0100

    lib underscore

--- src/app/pages/page-accueil/page-accueil.component.ts
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'underscore';
+import { list_products } from 'src/app/data';
 
 @Component({
   selector: 'app-page-accueil',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-accueil.component.scss']
 })
 export class PageAccueilComponent implements OnInit {
+  listData = list_products;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    let toto = _.uniq(this.listData, categorie => categorie.product_breadcrumb_label);
+    console.log(toto);
   }
 
 }
